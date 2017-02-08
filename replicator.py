74d53f9577cecd7efd0fe5f38eddbf1f27cf1978
Eva Connors <econnors@zacks.com>
2017-02-08 12:36:22 -0600

    broken and needs testing on the original env

--- replicator.py
+++ replicator.py
@@ -5,77 +5,65 @@
 
 import logging
 from logging.config import dictConfig
-from fabric.api import *
-from fabric.tasks import execute
-from fabric.state import env, output
+#from fabric.api import *
+#from fabric.tasks import execute
+#from fabric.state import env, output
 import os
-import libzfs_core
-import yaml
-
+#import libzfs_core
+#import yaml
+import sys
 
 #
 # Bootstrap
 #
-app_root = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(app_root, 'conf/config.yml')
-if not os.path.isfile(config_path):
-    print "ERROR - Config file not found at: " + config_path + ". Exiting"
-    exit()
+#app_root = os.path.dirname(os.path.abspath(__file__))
+#config_path = os.path.join(app_root, 'conf/config.yml')
+#if not os.path.isfile(config_path):
+#    print "ERROR - Config file not found at: " + config_path + ". Exiting"
+#    exit()
 
 
 #
 # Load config
 #    TODO: Exit on failure
-with open(config_path, 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+#with open(config_path, 'r') as ymlfile:
+#    cfg = yaml.load(ymlfile)
 
 
 #
 # Get logger
 #    TODO: Exit on failure
-logging.config.dictConfig(cfg['logging'])
-log = logging.getLogger('replicator_log')
-
-
-
+#logging.config.dictConfig(cfg['logging'])
+#log = logging.getLogger('replicator_log')
 
 #
-# Test function using Fabric
+# Variabe
 #
-def dataset_exists(dataset):
+
+if_snapshots = sys.argv[1]
+
+#
+# Functions
+#
+def __init__(dataset_name):
+    self.dataset_name = dataset_name
+
+def zfs_list(if_snapshots):
     
-    ret_list = []
+    returned_list = []
+    if_snapshots = ""
 
-    try:
-        # Fabric context to silence verbose messages
-        # and not abort on command failure
-        with quiet():
-            ret = local('zfs list -H -o name ' + dataset, capture=True)
+    returned = local('zfs list -H -o name ' + self.dataset_name + ' ' + if_snapshots)
 
-        # Build output struct
-        for line in ret.splitlines():
-            ret_list.append(line)
+    for line in returned.splitlines():
+        returned_list.append(line)
 
-        # Struct should contain exactly one line and match
-        # the input dataset name
-        if len(ret_list) == 1 and dataset == ret_list[0]:
-            return True
-
-    except FabricException:
-        return False
-
-
-
+    return returned_list
 
 #
 # Main
 #
-if dataset_exists("tank/test_dataset_1"):
-    log.info("FOUND DATASET")
-else:
-    log.error("DATASET NOT FOUND")
 
-
-
+returned_list = zfs_list(if_snapshots)
 
 
